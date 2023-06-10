623de4e94f635d60e6cb4dfe4a7680306077662d
Churnobyl <tjdcjfals@gmail.com>
2023-06-10 20:21:08 +0900

    :sparkles:Feat: 로그인 방식 수정

--- script/api.js
+++ script/api.js
@@ -3,33 +3,34 @@ import { BACK_BASE_URL, FRONT_BASE_URL } from "./conf.js";
 export async function signupApi(data) {
   // 회원가입 api
   const response = await fetch(`${BACK_BASE_URL}/users/`, {
-    headers:{
-      'content-type':'application/json',
+    headers: {
+      "content-type": "application/json",
     },
     method: "POST",
     body: JSON.stringify(data),
   });
+
   const responseJson = await response.json();
-  console.log(responseJson);
+  return { response, responseJson };
 }
 
 export async function loginApi(data) {
-  console.log(data)
+  console.log(data);
   const response = await fetch(`${BACK_BASE_URL}/users/login/`, {
-    headers:{
-      'content-type':'application/json',
+    headers: {
+      "content-type": "application/json",
     },
     method: "POST",
     body: JSON.stringify(data),
   });
   const responseJson = await response.json();
-  console.log(responseJson);
+  return responseJson;
 }
 
 export async function getQuizApi(data) {
   // 퀴즈 가져오기 api
-  const response = await fetch(`${BACK_BASE_URL}`)
+  const response = await fetch(`${BACK_BASE_URL}`);
   const responseJson = await response.json();
-  
-  return responseJson
+
+  return responseJson;
 }
--- script/battle/battle-page.js
+++ script/battle/battle-page.js
@@ -2,6 +2,9 @@ import { BACK_WEBSOCKET_URL } from "/script/conf.js";
 
 const urlParams = new URLSearchParams(window.location.search);
 const roomName = urlParams.get("room");
+const roomData = {
+  host: "김철수",
+};
 
 const chatSocket = new WebSocket(
   "ws://" + BACK_WEBSOCKET_URL + "/ws/battle/" + roomName + "/"
@@ -19,17 +22,21 @@ chatSocket.onclose = function (e) {
 document.getElementById("chat-message-input").focus();
 document.getElementById("chat-message-input").onkeyup = function (e) {
   if (e.key === 13 || e.key === "Enter") {
-    document.getElementById("chat-message-submit").click();
+    sendMessage();
   }
 };
-
 document.getElementById("chat-message-submit").onclick = function (e) {
+  sendMessage();
+};
+
+function sendMessage() {
   const messageInputDom = document.getElementById("chat-message-input");
   const message = messageInputDom.value;
   chatSocket.send(
     JSON.stringify({
+      roomData: roomData,
       message: message,
     })
   );
-};
-messageInputDom.value = "";
+  messageInputDom.value = "";
+}
